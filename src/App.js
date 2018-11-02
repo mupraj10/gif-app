7a7be61679cb544d59380f364f059221246107fb
Maggy Prajapati <mupraj10@gmail.com>
2018-11-02 13:42:32 -0400

    Adds in redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
