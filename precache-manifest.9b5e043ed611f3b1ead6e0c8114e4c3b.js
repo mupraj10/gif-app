882aa2ba08c35fada6ac6aee171743a59b285911
Maggy Prajapati <mupraj10@gmail.com>
2018-11-08 18:06:13 -0500

    Updates

---
+++ precache-manifest.9b5e043ed611f3b1ead6e0c8114e4c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "234acd4d39f67105ea6e",
+    "url": "/gif-app/static/css/main.6e84fa5b.chunk.css"
+  },
+  {
+    "revision": "234acd4d39f67105ea6e",
+    "url": "/gif-app/static/js/main.234acd4d.chunk.js"
+  },
+  {
+    "revision": "d0cf80009904707380e2",
+    "url": "/gif-app/static/js/1.d0cf8000.chunk.js"
+  },
+  {
+    "revision": "9a65d1699e489c47ba39",
+    "url": "/gif-app/static/js/runtime~main.9a65d169.js"
+  },
+  {
+    "revision": "55c54ba32818ec5b6b60a80964cdd730",
+    "url": "/gif-app/index.html"
+  }
+];
